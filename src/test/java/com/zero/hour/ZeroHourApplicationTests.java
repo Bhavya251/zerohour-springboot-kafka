0477e66c2d81ce9113b9e2d9c2a12565f9fe9c91
Bhavya <bhavyapatel.cs@gmail.com>
2025-09-21 20:28:17 -0600

    initial commit

---
+++ src/test/java/com/zero/hour/ZeroHourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.hour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroHourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
